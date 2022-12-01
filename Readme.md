f6894b5f879415c9f401306bf39ca5b42ffc62d3
Oleg Bogdanov <boguscoder@gmail.com>
2022-11-30 23:29:11 -0800

    mqtt experiments

--- Readme.md
+++ Readme.md
@@ -3,7 +3,14 @@ Start with apt deps:
 apt install influxdb
 apt install python3
 apt install python3-pip
+apt install mosquitto mosquitto-clients
 ```
+Now you can start both `infludb` and `mqtt` services:
+```
+sudo systemctl enable mosquitto.service
+sudo systemctl enable influxdb.service
+```
+
 This will be required to build `RPi.GPIO` locally
 ```
 apt install gcc
@@ -14,15 +21,22 @@ And then python deps:
 ```
 pip3 install pms5003
 pip3 install RPi.GPIO
+pip3 install paho-mqtt
 ```
 
-Dont forget to add `real_aqi.py` to cron
 
-For UI pick some handy folder:
+Next, you can run (manually or via cron) `real_aqi.py` to collect data and publish it to mqtt broker
+
+---
+
+For data visualization pick some handy folder:
 ```
 wget https://dl.influxdata.com/chronograf/releases/chronograf-1.10.0_linux_arm64.tar.gz
 tar xvfz chronograf-1.10.0_linux_arm64.tar.gz
 ```
 
-When building Dashboard check [airnow](https://www.airnow.gov/sites/default/files/2020-05/aqi-technical-assistance-document-sept2018.pdf) for range mapping between ppm and AQI
+Launch `chronograf` and import `Home AQI.json` dashboard or check [airnow](https://www.airnow.gov/sites/default/files/2020-05/aqi-technical-assistance-document-sept2018.pdf) for range mapping between ppm and AQI and create new one (`queries.sql` may be handy still)
 
+---
+
+WIP: follow Home Assistant Core [installation](https://www.home-assistant.io/installation/raspberrypi/#install-home-assistant-core)
--- real_aqi.py
+++ real_aqi.py
@@ -1,61 +1,82 @@
 from influxdb import InfluxDBClient
 import time
+import random
 from pms5003 import PMS5003
+from paho.mqtt import client as mqtt_client
 
-client = None
-pms5003 = None
 counter = 0
 
+def connect_mqtt():
+    client_id = f'python-mqtt-{random.randint(0, 1000)}'
+    
+    def cb(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.on_connect = cb
+    client.connect("localhost", 1883)
+    return client
+
+def connect_influxdb():
+  client = InfluxDBClient(host="localhost", port=8086)
+  client.create_database("aqi_real")
+  client.switch_database("aqi_real")
+  return client
+
+def connect_pms5003():
+  pms5003 = PMS5003(
+      device='/dev/ttyAMA0',
+      baudrate=9600,
+      pin_enable=22,
+      pin_reset=27
+  )
+  return pms5003
+
+
 def init():
-	global client, pms5003, counter
-	client = InfluxDBClient(host="localhost", port=8086)
-	client.create_database("aqi_real")
-	client.switch_database("aqi_real")
+  return (connect_influxdb(), connect_mqtt(), connect_pms5003())
 
-	pms5003 = PMS5003(
-	    device='/dev/ttyAMA0',
-	    baudrate=9600,
-	    pin_enable=22,
-	    pin_reset=27
-	)
-
-	counter = 0
-
-
-def tick():
-	global client, pms5003, counter
-	try:
-		data = pms5003.read()
-		points = [
-		  {
-		    "measurement" : "aqi",
-		    "fields": {
-		      "pm1": data.pm_ug_per_m3(1.0),
-		      "pm2.5": data.pm_ug_per_m3(2.5),
-		      "pm10": data.pm_ug_per_m3(10),
-		    }
-		  }
-		]
-		client.write_points(points)
-		if counter > 60:
-			counter = 0
-			pms5003.reset()
-		else:
-			counter = counter + 1
-		time.sleep(10)
-	except Exception as err:
-		error = [
-	          {
-	            "measurement" : "aqi_error",
-	            "fields": {
-        	      "reason": str(err),
-	            }
-	          }
-		]
-		client.write_points(error)
-		init() # reinit and pray
-
+def tick(influxdb, mqtt, pms5003):
+  global counter
+  try:
+    data = pms5003.read()
+    points = [
+      {
+        "measurement" : "aqi",
+        "fields": {
+          "pm1": data.pm_ug_per_m3(1.0),
+          "pm2.5": data.pm_ug_per_m3(2.5),
+          "pm10": data.pm_ug_per_m3(10),
+        }
+      }
+    ]
+    influxdb.write_points(points)
+    
+    if data.pm_ug_per_m3(2.5) >= 10:
+      mqtt.publish("/home/pm", "ON")
+    elif data.pm_ug_per_m3(2.5) <= 5:
+      mqtt.publish("/home/pm", "OFF")
+    
+    if counter > 60:
+      counter = 0
+      pms5003.reset()
+    else:
+      counter = counter + 1
+    time.sleep(10)
+  except Exception as err:
+    error = [
+            {
+              "measurement" : "aqi_error",
+              "fields": {
+                "reason": str(err),
+              }
+            }
+    ]
+    influxdb.write_points(error)
 
 if __name__ == '__main__':
-	init()
-	while True: tick()
+  (influxdb, mqtt, pms5003) = init()
+  while True: tick(influxdb, mqtt, pms5003)
